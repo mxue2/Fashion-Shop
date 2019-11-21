2ec3b30157e695e6ca4ccf4f75c8f18a8d11ab30
Olivia <mxue2@student.unimelb.edu.au>
2019-11-21 21:11:04 +1100

    Updates

---
+++ precache-manifest.073642f13da4b2dff48c20282ae12efd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484b928d1442e2a0bb34be014a6e160b",
+    "url": "/Fashion-Shop/index.html"
+  },
+  {
+    "revision": "5719c9f85587c98b57fe",
+    "url": "/Fashion-Shop/static/css/main.9228da63.chunk.css"
+  },
+  {
+    "revision": "27caffc3951881196d9b",
+    "url": "/Fashion-Shop/static/js/2.1778bc92.chunk.js"
+  },
+  {
+    "revision": "5719c9f85587c98b57fe",
+    "url": "/Fashion-Shop/static/js/main.04d386b2.chunk.js"
+  },
+  {
+    "revision": "891fe7187d79afaf0e64",
+    "url": "/Fashion-Shop/static/js/runtime-main.163f4b68.js"
+  },
+  {
+    "revision": "54e2d12f1bf360070cdc4cf3d3b9255e",
+    "url": "/Fashion-Shop/static/media/denim_wash_shirt.54e2d12f.jpg"
+  },
+  {
+    "revision": "790540d3657cc056c29a80d70eaaa3b8",
+    "url": "/Fashion-Shop/static/media/plaid_shirt.790540d3.jpg"
+  },
+  {
+    "revision": "c7281004ad4212ed49e8a18922777760",
+    "url": "/Fashion-Shop/static/media/raglan_tee.c7281004.jpg"
+  },
+  {
+    "revision": "89b685a638921571188c7f4d2e32eac9",
+    "url": "/Fashion-Shop/static/media/rugby_jersey.89b685a6.jpg"
+  },
+  {
+    "revision": "01847be4a0fd5bf61fcc19543e3fdc7c",
+    "url": "/Fashion-Shop/static/media/staple_stripe.01847be4.jpg"
+  },
+  {
+    "revision": "d23dc29a3f34553480fd2709ab0a0e91",
+    "url": "/Fashion-Shop/static/media/stone_wash.d23dc29a.jpg"
+  }
+]);
